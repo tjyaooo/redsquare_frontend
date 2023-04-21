f0939391b20ea7fba131481a722a7bbe63e73e1a
tjyaooo <tjyaooo@gmail.com>
2023-04-22 05:28:53 +0800

    configure app page navigation

--- question2_folder/client/src/components/ProductPage.js
+++ question2_folder/client/src/components/ProductPage.js
@@ -6,10 +6,12 @@ import {
   Img,
   Flex,
   Center,
-
   HStack,
+  Button,
+  Divider,
+  Select
 } from '@chakra-ui/react';
-import { BsArrowUpRight, BsHeartFill, BsHeart } from 'react-icons/bs';
+import { BsArrowUpRight, BsFillCartPlusFill } from 'react-icons/bs';
 
 
 import React, { useState, useEffect } from 'react';
@@ -17,23 +19,54 @@ import React, { useState, useEffect } from 'react';
 export default function ProductPage() {
   const [liked, setLiked] = useState(false);
   const [products, setProducts] = useState([]);
+  const [pageNo, setPageNo] = useState(1);
+  const [prevPageButtonDisable, setPrevDisable] = useState(true);
+  const [nextPageButtonDisable, setNextDisable] = useState(false);
+  
+  // if(pageNo === 1){
+  //   setPrevDisable(true)
+  // } else{
+  //   setPrevDisable(false)
+  // }
+
+  // if(pageNo === products.length){
+  //   setPrevDisable(true)
+  // } else{
+  //   setPrevDisable(false)
+  // }
 
   useEffect(() => {
     fetch('https://dummyjson.com/products')
       .then(res => res.json())
-      .then(data => setProducts(data.products))
+      .then(data => {
+        setProducts(data.products)
+      })
       .catch(error => console.log(error));
   }, []);
 
+  // for (let i=0; i < products.length; i+10) {
+  //   pagesArray.push(i+1);
+  // }
+  console.log(products.filter((_, index) => index % 10 === 0))
+
+  const handleNextPage=() => {
+    setPageNo(pageNo+1)
+  }
+  const handlePreviousPage=() => {
+    setPageNo(pageNo-1)
+  }
+  const handleSelectPage=(pgno) => {
+    setPageNo(pgno)
+  }
+
   return (
-    <Flex px='10%' flexWrap={'wrap'} justifyContent='center'>
-      {products.map(product => (
+    <Flex px='10%' flexWrap={'wrap'} overflow='hidden'>
+      {products.slice((pageNo-1)*10,pageNo*10).map(product => (
           <Center py={6}>
           <Box
             w="23rem"
             h="26rem"
             rounded={'sm'}
-            my={5}
             mx={[0, 5]}
             overflow={'hidden'}
             bg="white"
@@ -46,7 +79,7 @@ export default function ProductPage() {
                   product.images[product.images.length-1]
                 }
                 roundedTop={'sm'}
-                objectFit="cover"
+                objectFit="scale-down"
                 h="full"
                 w="full"
                 // maxW={'100%'}
@@ -95,94 +128,31 @@ export default function ProductPage() {
                 borderLeft={'1px'}
                 cursor="pointer"
                 onClick={() => setLiked(!liked)}>
-                {liked ? (
-                  <BsHeartFill fill="red" fontSize={'24px'} />
-                ) : (
-                  <BsHeart fontSize={'24px'} />
-                )}
+                {<BsFillCartPlusFill/>}
               </Flex>
             </HStack>
             </Flex>
           </Box>
         </Center>
       ))}
+      <Divider/>
+      <HStack width='80rem'>
+        <Button isDisabled={prevPageButtonDisable} onClick= {handlePreviousPage} colorScheme='teal' size='lg'>
+          Previous Page
+        </Button>
+        <Select  size='lg' >
+          <option hidden disabled value="">Select Page</option>
+        {
+          products.filter((_, index) => index % 10 === 0).map((value, index) => index + 1).map(idx => (
+            <option value={idx}>{idx}</option>
+          ))
+        }
+        
+        </Select>
+        <Button isDisabled={nextPageButtonDisable} onClick = {handleNextPage} colorScheme='teal' size='lg'>
+          Next Page
+        </Button>
+      </HStack>
     </Flex>
   );
-
-
-
-  // return (
-  //   <Center py={6}>
-  //     <Box
-  //       w="xs"
-  //       rounded={'sm'}
-  //       my={5}
-  //       mx={[0, 5]}
-  //       overflow={'hidden'}
-  //       bg="white"
-  //       border={'1px'}
-  //       borderColor="black"
-  //       boxShadow={useColorModeValue('6px 6px 0 black', '6px 6px 0 cyan')}>
-  //       <Box h={'200px'} borderBottom={'1px'} borderColor="black">
-  //         <Img
-  //           src={
-  //             product.images[0]
-  //           }
-  //           roundedTop={'sm'}
-  //           objectFit="cover"
-  //           h="full"
-  //           w="full"
-  //           alt={'Image'}
-  //         />
-  //       </Box>
-  //       <Box p={4}>
-  //         <Box
-  //           bg="black"
-  //           display={'inline-block'}
-  //           px={2}
-  //           py={1}
-  //           color="white"
-  //           mb={2}>
-  //           <Text fontSize={'xs'} fontWeight="medium">
-  //             {product.title}
-  //           </Text>
-  //         </Box>
-  //         <Heading color={'black'} fontSize={'2xl'} noOfLines={1}>
-  //           React v18.0
-  //         </Heading>
-  //         <Text color={'gray.500'} noOfLines={2}>
-  //           {product.description}
-  //         </Text>
-  //       </Box>
-  //       <HStack borderTop={'1px'} color="black">
-  //         <Flex
-  //           p={4}
-  //           alignItems="center"
-  //           justifyContent={'space-between'}
-  //           roundedBottom={'sm'}
-  //           cursor={'pointer'}
-  //           w="full">
-  //           <Text fontSize={'md'} fontWeight={'semibold'}>
-  //             View more
-  //           </Text>
-  //           <BsArrowUpRight />
-  //         </Flex>
-  //         <Flex
-  //           p={4}
-  //           alignItems="center"
-  //           justifyContent={'space-between'}
-  //           roundedBottom={'sm'}
-  //           borderLeft={'1px'}
-  //           cursor="pointer"
-  //           onClick={() => setLiked(!liked)}>
-  //           {liked ? (
-  //             <BsHeartFill fill="red" fontSize={'24px'} />
-  //           ) : (
-  //             <BsHeart fontSize={'24px'} />
-  //           )}
-  //         </Flex>
-  //       </HStack>
-  //     </Box>
-  //   </Center>
-  // );
 }
--- question2_folder/client/src/components/Sidebar.js
+++ question2_folder/client/src/components/Sidebar.js
@@ -251,44 +251,6 @@ import {
     )
   }
   
-  const NAV_ITEMS = [
-    {
-      label: "Inspiration",
-      children: [
-        {
-          label: "Explore Design Work",
-          subLabel: "Trending Design to inspire you",
-          href: "#"
-        },
-        {
-          label: "New & Noteworthy",
-          subLabel: "Up-and-coming Designers",
-          href: "#"
-        }
-      ]
-    },
-    {
-      label: "Find Work",
-      children: [
-        {
-          label: "Job Board",
-          subLabel: "Find your dream design job",
-          href: "#"
-        },
-        {
-          label: "Freelance Projects",
-          subLabel: "An exclusive list for contract work",
-          href: "#"
-        }
-      ]
-    },
-    {
-      label: "Learn Design",
-      href: "#"
-    },
-    {
-      label: "Hire Designers",
-      href: "#"
-    }
-  ]
+  // to be populated
+  const NAV_ITEMS = []
   
